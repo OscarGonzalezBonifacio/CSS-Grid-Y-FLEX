3d8ebbeb7713506589333c3354377f808f95b98d
OscarGonzalezBonifacio <oscarglzatc@gmail.com>
2021-07-08 15:45:20 -0500

    finalizacion del menu vertical

--- Panel-administracion/js/app.js
+++ Panel-administracion/js/app.js
@@ -1,15 +1,16 @@
+/* seleciona documentos del HTMl queyselector */
 const menuIzquierdo = document.querySelector('.menu-izquierdo');
-
+/*comparamos */
 menuIzquierdo.addEventListener('click', (e)  => {
        const claseMenu = e.target.classList;
        
-       // Selecciona el contenedor
+       // Selecciona el contenedor, selecionamos variables
        const contenedor = document.querySelector('.pagina'),
              flechaIzq = document.querySelector('.fa-arrow-left'),
              flechaDer = document.querySelector('.fa-arrow-right');
 
        if(claseMenu.contains('fa-arrow-left') ) {
-            // cerrar el menú lateral
+            // aqui agregamos la clase no-menu
             contenedor.classList.add('no-menu');
             e.target.style.display = 'none';
             flechaDer.style.display = 'block';
